0e34f2d286b57ce5bea1f1955742a29c5641c63f
Benjamin Amoah <amoah.benjamin@gmail.com>
2019-07-05 00:09:34 -0400

    Fix the port for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require('cors');
 
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
         res.end();
     }
 })
-.listen(port, () => console.log("You are connected at port ${port}"));
+.listen(PORT, () => console.log("You are connected at port" + PORT));
 
 
 /*app.use(function(req, res, next) {
@@ -96,5 +96,5 @@ app.use((req, res, next) => {
     }
 });
 
-app.listen(port, () => console.log("You are connected at port ${port}"));
+app.listen(PORT, () => console.log("You are connected at port ${port}"));
 */

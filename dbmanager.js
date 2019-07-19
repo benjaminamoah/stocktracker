51c281caed88c587c24a776d43d7e0e316b71998
Benjamin Amoah <amoah.benjamin@gmail.com>
2019-07-19 15:00:02 -0400

    Integrating a mongodb database

---
+++ dbmanager.js
@@ -0,0 +1,27 @@
+//mangodb query
+const mongoclient = require('mongodb').MongoClient;
+
+function getAggregates(){ 
+    return new Promise((resolve, reject) => { 
+    //mongoclient.connect('mongodb://localhost:27017/stocktracker',
+    mongoclient.connect('mongodb+srv://bamoah:utopiamaya3@cluster0-xqp9f.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true },
+    function(err, client){
+        if(err) throw err;
+
+        const db = client.db('stocktracker');
+
+        db.collection('aggregates').find().toArray(
+            function(err, result){
+                if(err) reject(err)
+                
+                resolve(result);
+            }
+        )
+    });
+
+    
+});
+}
+
+module.exports.getAggregates = getAggregates;
--- stockchanges.js
+++ stockchanges.js
@@ -1,23 +1,44 @@
+//The Model in MVC
+const dm = require("./dbmanager.js");
 
-    function generateIndex(base) {
-        let change = Math.floor(Math.random() * 10);
-        let positiveotnegative = Math.random() < 0.5 ? -1 : 1;
-        let indexchange = Math.floor(base * (change/10000)) * positiveotnegative;
-        return indexchange;
+function generateIndex(base) {
+    let change = Math.floor(Math.random() * 10);
+    let positiveotnegative = Math.random() < 0.5 ? -1 : 1;
+    let indexchange = Math.floor(base * (change/10000)) * positiveotnegative;
+    return indexchange;
+}
+
+function retreiveaggregates(){
+    return new Promise(function(resolve, reject){
+        dm.getAggregates().then(function(data){
+            if(data.length > 0){
+                resolve(data); 
+            }else{
+                reject("Could not retreive data!");
+            }   
+        })
+        .catch(function(error){
+            console.log("Error message :"+error);
+        });
+    });
+}
+
+function aggregates(data){
+    for(i = 0; i < data.length; i++){
+        data[i].index = parseInt( data[i].index );
+        data[i].change = parseInt( generateIndex( data[i].index ) );
+        data[i].index += parseInt( data[i].change );
     }
+    return data;
+}
 
-    function aggregates(){
-        aggregate = {};
+function yourstocks(){
+    yourstocks = {};
 
-        return aggregate;
-    }
+    return yourstocks;
+}
 
-    function yourstocks(){
-        yourstocks = {};
-
-        return yourstocks;
-    }
-
-    module.exports.generateIndex = generateIndex;
-    module.exports.yourstocks = yourstocks;
-    module.exports.aggregates = aggregates;
+module.exports.generateIndex = generateIndex;
+module.exports.yourstocks = yourstocks;
+module.exports.retreiveaggregates = retreiveaggregates;
+module.exports.aggregates = aggregates;
